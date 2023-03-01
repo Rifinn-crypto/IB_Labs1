895ea8810cd84d40b1f4f6e2bc81b2b535c57d54
Nikita Raven <esh2015@list.ru>
2023-03-01 19:00:15 +0400

    Fuf

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 
-alfavit =  'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
-
+alfavit_RU = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
 smeshenie = int(input('Шаг шифровки: '))
-message = input("Сообщение для шифровки: ").upper()
 itog = ''
+with open("Text.txt", mode='r', encoding='utf-8') as T:
+    with open('Encryption.txt', mode='w', encoding='utf-8') as ET:
+        MyText = T.read().upper()
+        for i in MyText:
+            mesto = alfavit_RU.find(i)
+            new_mesto = mesto + smeshenie
+            if i in alfavit_RU:
+                 itog += alfavit_RU[new_mesto]
+            else:
+                itog += i
+        print(itog)
+        ET.write(itog)
