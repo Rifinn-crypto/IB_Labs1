e9c8cf23ea3fae6e4221dd80afcaeab1771fb49c
Nikita Raven <esh2015@list.ru>
2023-03-01 14:30:09 +0400

    we create a variable smeshenie, which will be manually set by the user, and message, where our message will be placed, and, using the upper() method, we raise all the characters in our message to uppercase so that we do not have errors. Then we create just an empty itog variable, where we will output the encrypted message.

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
+
 alfavit =  'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
+
+smeshenie = int(input('Шаг шифровки: '))
+message = input("Сообщение для шифровки: ").upper()
+itog = ''
